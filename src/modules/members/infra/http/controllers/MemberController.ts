77151b81b56abc83eda0e83f8d47d56c533d9019
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-09-25 12:33:24 -0300

    feat: add new member created

--- src/modules/members/infra/http/controllers/MemberController.ts
+++ src/modules/members/infra/http/controllers/MemberController.ts
@@ -1,7 +1,66 @@
 import { Request, Response } from "express";
+import ChurchRepository from "../../../../churchs/infra/prisma/repositories/ChurchRepository";
+import CreateNewMemberService from "../../../services/CreateNewMemberService";
+import MemberRepository from "../../prisma/repositories/MemberRepository";
+
+interface IRequest {
+  id_church: number;
+  name: string;
+  birth_date: Date;
+  batism_date: Date;
+  titleChurch: string;
+  cpf: bigint;
+  rg: number;
+  login: string;
+  email: string;
+  password: string;
+}
 
 export default class MemberController {
-  async create (request: Request, response: Response) {
-    
+  async create(request: Request, response: Response) {
+    try {
+      const {
+        batism_date,
+        birth_date,
+        cpf,
+        email,
+        login,
+        name,
+        password,
+        rg,
+        titleChurch,
+        id_church,
+      }: IRequest = request.body;
+  
+      const memberRepository = new MemberRepository();
+      const churchRepository = new ChurchRepository();
+      const createNewMember = new CreateNewMemberService(
+        memberRepository,
+        churchRepository
+        );
+        
+        let member = await createNewMember.execute({
+        batism_date,
+        birth_date,
+        cpf,
+        email,
+        login,
+        name,
+        password,
+        rg,
+        titleChurch,
+        id_church,
+      });
+
+      // @ts-ignore
+      // It's just to can return in JSON.
+      if (member) member.cpf = member.cpf.toString();
+  
+      return response.json({ member });
+    } catch (error) {
+      if (error instanceof Error) {
+        return response.status(401).json({error: error.message})
+      }
+    }
   }
 }
--- src/modules/members/services/CreateNewMemberService.ts
+++ src/modules/members/services/CreateNewMemberService.ts
@@ -1,5 +1,37 @@
+import { confirmIsDate } from "../../../shared/utils/confirmIsDate";
+import { IChurchRepository } from "../../churchs/repositories/IChurchRepository";
+import { ICreateMemberDTO } from "../dtos/ICreateMemberDTO";
+import { IMemberRepository } from "../repositories/IMemberRepository";
+
 export default class CreateNewMemberService {
-  public async execute() {
+  constructor(private memberRepository: IMemberRepository, private churchRepository:IChurchRepository) {
+    this.memberRepository = memberRepository;
+    this.churchRepository = churchRepository;
+  }
+
+  public async execute(dataMember: ICreateMemberDTO) {
+    if (dataMember.id_church === undefined)
+      throw new Error("id_church pass undefined");
     
+    const church = await this.churchRepository.findById(dataMember.id_church);
+    if(!church) throw new Error("Church doesn't exist");
+
+    if (confirmIsDate(dataMember.birth_date))
+      dataMember.birth_date = new Date(dataMember.birth_date);
+    else throw new Error("Birth date format is incorrect (yyyy-mm-dd)");
+
+    if (confirmIsDate(dataMember.batism_date))
+      dataMember.batism_date = new Date(dataMember.batism_date);
+    else throw new Error("Batism date format is incorrect (yyyy-mm-dd)");
+
+    if (dataMember.cpf.toString().length !== 11)
+      throw new Error("CPF format is incorrect");
+
+    if (dataMember.birth_date.toString() === dataMember.batism_date.toString())
+      throw new Error("Birth date and Batism date can not be equals");
+    
+    const member = await this.memberRepository.create(dataMember);
+
+    return member;
   }
 }
