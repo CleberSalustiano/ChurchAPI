2cb920e74b2f136da7d8316b0cf24c4eb3f70793
CleberSalustiano <kleberdejesus45@hotmail.com>
2023-02-19 22:36:57 -0300

    feat: specialOffer route created

--- src/modules/specialOffer/infra/http/routes/specialOffer.routes.ts
+++ src/modules/specialOffer/infra/http/routes/specialOffer.routes.ts
@@ -1,4 +1,9 @@
 import { Router } from "express";
+import SpecialOfferController from "../controllers/SpecialOfferController";
 
 const specialOfferRouter = Router();
+const specialOfferController = new SpecialOfferController(); 
 
+specialOfferRouter.post("/", specialOfferController.create);
+
+export default specialOfferRouter;
